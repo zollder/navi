534d69f7413d9f63964b0443f5113e68453f1bad
Eugene Simon <eugen@publicbikesystem.com>
2013-10-24 22:28:20 -0400

    DAP measurements: only DAP thread enabled

--- AcceleratorThread.cc
+++ AcceleratorThread.cc
@@ -46,6 +46,12 @@
 	//-----------------------------------------------------------------------------------------
 	void* AcceleratorThread::run()
 	{
+		// initialize time tracking variables
+		float elapse = 0;
+		float bcet = 1;
+		float wcet = 0;
+		float accumulator = 0;
+
 		// dummy buffer
 		string buffer[8];
 
@@ -103,11 +109,25 @@
 			    float elapse = ( last_cycles - current_cycles) / cpu_freq;
 		    	printf("Accelerator Execution time %f seconds\n", elapse );
 
+		    	// worst execution time tracker
+		    	if (wcet < elapse)
+		    		wcet = elapse;
+		    	// best execution time tracker
+		    	if (bcet > elapse)
+		    		bcet = elapse;
+		    	// exec time accumulator
+		    	accumulator = accumulator + elapse;
+
 		    	counter++;
 			}
 		}
 
 		printf("\nAcceleratorThread %lu done at %d seconds\n", (long unsigned int)getThreadId() , time(NULL)-startTime );
+
+		// show time measurements
+		printf("\nWorst-case execution time: %f", wcet);
+		printf("\nBest-case execution time: %f", bcet);
+		printf("\nAverage execution time: %f\n", accumulator/(duration*3));
 
 		return NULL;
 	}
--- navi.cc
+++ navi.cc
@@ -16,31 +16,31 @@ int main(void)
 	NaviData* naviData = new NaviData();
 
 	// creating DAP and DD threads
-	DisplayThread* displayThread = new DisplayThread(mutex, naviData);
+	//DisplayThread* displayThread = new DisplayThread(mutex, naviData);
 	AcceleratorThread* acceleratorThread = new AcceleratorThread(mutex, naviData);
 
 	// creating timers for each thread with specified pulse interval
-	PulseTimer* displayTimer = new PulseTimer(6, displayThread->getChannelId());
+	//PulseTimer* displayTimer = new PulseTimer(6, displayThread->getChannelId());
 	PulseTimer* acceleratorTimer = new PulseTimer(2, acceleratorThread->getChannelId());
 
 	printf("\n\n\t-------------Begin---------------\n\n");
 
 	// start timers and threads
-	displayThread->start();
-	displayTimer->start();
+	//displayThread->start();
+	//displayTimer->start();
 	acceleratorThread->start();
 	acceleratorTimer->start();
 
 	// terminate threads
-	displayThread->join();
+	//displayThread->join();
 	acceleratorThread->join();
 
 	printf("\n\n\t-------------END-----------------\n\n");
 
 	// cleanup
-	delete displayTimer;
+	//delete displayTimer;
 	delete acceleratorTimer;
-	delete displayThread;
+	//delete displayThread;
 	delete acceleratorThread;
 	delete naviData;
 
